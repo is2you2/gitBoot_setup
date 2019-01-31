4c08ca6bfae8024a324a7ea76d3b4631c144610a
is2you2 <liss22@hanmail.net>
2019-01-31 23:23:10 +0900

    someCodes

--- firstRun.sh
+++ firstRun.sh
@@ -1,4 +1,4 @@
 sudo apt install xvfb libxrender1 libxtst6 libxi6 default-jdk unzip zip -y
-chmod 700 startgit.bash
-./startgit.bash
-rm firstRun.bash
+chmod 700 startgit.sh
+./startgit.sh
+rm firstRun.sh
